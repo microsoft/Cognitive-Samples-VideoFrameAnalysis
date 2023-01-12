dddcb003e3130e0c24964d062fd59e80a7fcb678
Leo.Lu <lel@microsoft.com>
2023-01-12 13:45:09 +0800

    Remove sensitive attributes and set returnFaceId to false
    New users don't have permission to use senstive attributes and recognition functionalities
    So we remove these feature in the demo to ensure that new users can run this demo successfully

--- Windows/LiveCameraSample/Aggregation.cs
+++ Windows/LiveCameraSample/Aggregation.cs
@@ -34,41 +34,22 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using Microsoft.Azure.CognitiveServices.Vision.Face.Models;
 
 namespace LiveCameraSample
 {
     internal class Aggregation
     {
-        public static KeyValuePair<string, double> GetDominantEmotion(Emotion scores)
-        {
-            return new Dictionary<string, double>()
-                {
-                    { "Anger", scores.Anger },
-                    { "Contempt", scores.Contempt },
-                    { "Disgust", scores.Disgust },
-                    { "Fear", scores.Fear },
-                    { "Happiness", scores.Happiness },
-                    { "Neutral", scores.Neutral },
-                    { "Sadness", scores.Sadness },
-                    { "Surprise", scores.Surprise }
-                }
-                .OrderByDescending(kv => kv.Value)
-                .ThenBy(kv => kv.Key)
-                .First();
-        }
-
-        public static string SummarizeEmotion(Emotion scores)
-        {
-            var bestEmotion = Aggregation.GetDominantEmotion(scores);
-            return string.Format("{0}: {1:N1}", bestEmotion.Key, bestEmotion.Value);
-        }
-
         public static string SummarizeFaceAttributes(FaceAttributes attr)
         {
             List<string> attrs = new List<string>();
-            if (attr.Gender.HasValue) attrs.Add(attr.Gender.Value.ToString());
-            if (attr.Age > 0) attrs.Add(attr.Age.ToString());
+
+            if (attr.Glasses != null)
+            {
+                attrs.Add($"Glasses: {attr.Glasses}");
+            }
+
             if (attr.HeadPose != null)
             {
                 // Simple rule to estimate whether person is facing camera. 
